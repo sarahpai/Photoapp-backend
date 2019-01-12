2eb177707691bc0a131faa2181d0e6bdbd701c07
Sarah Pai <pai.sarah89@gmail.com>
2019-01-12 06:40:40 -0500

    login error fixed

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::AuthController < ApplicationController
 	  if @user && @user.authenticate(user_login_params[:password])
 			# encode token comes from ApplicationController
 			@token = encode_token({ user_id: @user.id })
-				render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+				render json: { user: UserSerializer.new(@user), jwt: @token }, status: :accepted
 	  else
 				render json: { message: 'Invalid username or password' }, status: :unauthorized
 	  end
